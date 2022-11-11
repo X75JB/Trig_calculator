9f0723cc729a4636011888dd41dcc33bfa169a75
Jackson Blackman <80335379+X75JB@users.noreply.github.com>
2022-11-11 13:44:20 -0500

    Create trig_calculator_v2.py

---
+++ trig_calculator_v2.py
@@ -0,0 +1,90 @@
+# Trig Calculator
+# Jackson Blackman
+# 11_07_22
+
+from tkinter import Label, Button, Entry
+from math import radians, sin, cos, tan, asin, acos, atan, degrees
+
+
+# Functions
+## Calculating ratios
+def calculate_ratios(angle):
+    try:
+        sine_ratio = round(sin(radians(float(angle))), 4)
+        cosine_ratio = round(cos(radians(float(angle))), 4)
+        tan_ratio = round(tan(radians(float(angle))), 4)
+
+        sine_ratio_output.config(text=sine_ratio)
+        cosine_ratio_output.config(text=cosine_ratio)
+        tan_ratio_output.config(text=tan_ratio)
+
+    except:
+        sine_ratio_output.config(text='Error')
+        cosine_ratio_output.config(text='Error')
+        tan_ratio_output.config(text='Error')
+
+
+## Calculating inverse
+def calculate_inverse(ratio):
+    try:
+        inverse_sine_ratio = round(sin(degrees(asin(float(ratio)))), 4)
+        inverse_cosine_ratio = round(cos(degrees(acos(float(ratio)))), 4)
+        inverse_tan_ratio = round(tan(degrees(atan(float(ratio)))), 4)
+
+        inverse_sine_ratio_output.config(text=inverse_sine_ratio)
+        inverse_cosine_ratio_output.config(text=inverse_cosine_ratio)
+        inverse_tan_ratio_output.config(text=inverse_tan_ratio)
+
+    except:
+        inverse_sine_ratio_output.config(text='Error')
+        inverse_cosine_ratio_output.config(text='Error')
+        inverse_tan_ratio_output.config(text='Error')
+
+
+# Gathering starting values
+Label(text='Enter an angle in a right triangle: ').grid(row=0, column=0)
+angle_entry = Entry()
+angle_entry.grid(row=0, column=1)
+angle_entry.focus_set()
+
+## Calculation button
+ratio_button = Button(text='Calculate Trig Ratios',
+                      command=lambda: calculate_ratios(angle_entry.get()))
+ratio_button.grid(row=1, column=0)
+
+# State final ratios
+Label(text='The sine ratio is: ').grid(row=2, column=0)
+sine_ratio_output = Label()
+sine_ratio_output.grid(row=2, column=1)
+
+Label(text='The cosine ratio is: ').grid(row=3, column=0)
+cosine_ratio_output = Label()
+cosine_ratio_output.grid(row=3, column=1)
+
+Label(text='The tan ratio is: ').grid(row=4, column=0)
+tan_ratio_output = Label()
+tan_ratio_output.grid(row=4, column=1)
+
+# Gathering starting values - inverse
+Label(text='Enter an ratio after dividing sides: ').grid(row=5, column=0)
+ratio_entry = Entry()
+ratio_entry.grid(row=5, column=1)
+ratio_entry.focus_set()
+
+## Inverse Calculation button
+angle_button = Button(text='Calculate Angle',
+                      command=lambda: calculate_inverse(ratio_entry.get()))
+angle_button.grid(row=6, column=0)
+
+# State final angles
+Label(text='The sine angle opp/hyp: ').grid(row=7, column=0)
+inverse_sine_ratio_output = Label()
+inverse_sine_ratio_output.grid(row=7, column=1)
+
+Label(text='The cosine angle adj/hyp: ').grid(row=8, column=0)
+inverse_cosine_ratio_output = Label()
+inverse_cosine_ratio_output.grid(row=8, column=1)
+
+Label(text='The tan angle opp/adj: ').grid(row=9, column=0)
+inverse_tan_ratio_output = Label()
+inverse_tan_ratio_output.grid(row=9, column=1)
